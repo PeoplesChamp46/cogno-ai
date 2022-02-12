d1952ca2333b5b8f629084a59bc9c312aad60ce1
PeoplesChamp46 <sasukemestry@gmail.com>
2022-02-13 02:48:16 +0530

    responsive

--- animation.js
+++ animation.js
@@ -1,6 +1,6 @@
 let tl = gsap.timeline({defaults: { Easings : Expo.EaseOut}});
 
-tl.from('.bot-head', {duration : 1, opacity : 0, delay: 0.2})
+tl.from('.bot-head', {duration : 1, opacity : 0, delay: 0.1})
   .from('.heading, .subheading, .title, #hello, #inner', { duration: 1, opacity: 0})
   .from('.bot-content', { duration: 1, opacity: 0})
-  .from('.content-box, .button-container', { duration: 1, opacity: 0, stagger: 0.8})
+  .from('.content-box, .button-container', { duration: 1, opacity: 0, stagger: 0.6})
